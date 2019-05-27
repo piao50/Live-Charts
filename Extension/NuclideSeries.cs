5177b04c9c67ecc89df50b11f6e2ee53e2597a7a
Gu Shi <piao50@gmail.com>
2019-05-27 21:27:30 +0800

    11

---
+++ Extension/NuclideSeries.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using LiveCharts.Definitions.Points;
+using LiveCharts.Definitions.Series;
+using LiveCharts.Dtos;
+using LiveCharts.SeriesAlgorithms;
+using LiveCharts.Wpf.Charts.Base;
+using LiveCharts.Wpf.Points;
+
+namespace LiveCharts.Wpf
+{
+	public class NuclideSeries : Series
+	{
+		
+	}
+}
